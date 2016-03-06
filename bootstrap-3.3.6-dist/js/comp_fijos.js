04b62cbe7f690817deed6a776d6979792ee184ef
dvillatobas <dvd1880@gmail.com>
2016-03-06 12:59:02 +0100

    Estructura de pantalla principal por componentes.

---
+++ bootstrap-3.3.6-dist/js/comp_fijos.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('header').load('componentes/header.html');
+    $('aside').load('componentes/aside.html');
+    $('#contenido').load('componentes/contenido.html');
+    $('footer').load('componentes/footer.html');
+});
