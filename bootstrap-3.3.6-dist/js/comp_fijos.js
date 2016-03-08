9e54b3d29dbf43dce1d45caabc395d515c132260
CarlosSantos8 <4a.santos@gmail.com>
2016-03-08 12:46:41 +0100

    Version 0

--- bootstrap-3.3.6-dist/js/comp_fijos.js
+++ bootstrap-3.3.6-dist/js/comp_fijos.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    $('header').load('componentes/header.html');
+$(document).ready(function () {
+    $('nav').load('componentes/header.html');
     $('aside').load('componentes/aside.html');
     $('#contenido').load('componentes/contenido.html');
     $('footer').load('componentes/footer.html');
