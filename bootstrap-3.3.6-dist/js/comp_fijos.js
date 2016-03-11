57c39764c45dcad607d136d18c5e9a77e3f2de66
CarlosSantos8 <4a.santos@gmail.com>
2016-03-11 12:13:04 +0100

    Header y footer realizados

--- bootstrap-3.3.6-dist/js/comp_fijos.js
+++ bootstrap-3.3.6-dist/js/comp_fijos.js
@@ -2,5 +2,5 @@ $(document).ready(function () {
     $('nav').load('componentes/header.html');
     $('aside').load('componentes/aside.html');
     $('#contenido').load('componentes/contenido.html');
-    $('footer').load('componentes/footer.html');
+    $('.navbar-foot').load('componentes/footer.html');
 });
