3369b941a681c788df8d376d1b743fb5dc9972e9
dvillatobas <dvd1880@gmail.com>
2016-04-18 22:59:18 +0200

    cambio de proyecto para que se autorrefresque

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -1,9 +1,15 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from './app/app.component';
 import {provide} from 'angular2/core';
-import {RouteConfig,  ROUTER_DIRECTIVES, ROUTER_PROVIDERS,
-        LocationStrategy, HashLocationStrategy} from 'angular2/router';
+import {
+	ROUTER_PROVIDERS,
+	RouteConfig,  
+	ROUTER_DIRECTIVES,
+	LocationStrategy, 
+	HashLocationStrategy
+} from 'angular2/router';
 
 bootstrap(AppComponent, [
-  ROUTER_PROVIDERS, provide(LocationStrategy, {useClass: HashLocationStrategy})
+  ROUTER_PROVIDERS, 
+  provide(LocationStrategy, {useClass: HashLocationStrategy})
 ]);
--- angular/src/typings.d.ts
+++ angular/src/typings.d.ts
@@ -1,1 +1,1 @@
-/// <reference path="../node_modules/angular2/typings/browser.d.ts" />
+/// <reference path="../typings/browser.d.ts" />
