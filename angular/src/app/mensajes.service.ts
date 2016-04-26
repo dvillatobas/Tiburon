b9eb132c44900d39afbf4cb717f3776e8cd6ac61
cvs4190 <cvs_4190@hotmail.com>
2016-04-26 10:35:05 +0200

    cambios en mensajes
    cambios en texto

--- angular/src/app/mensajes.service.ts
+++ angular/src/app/mensajes.service.ts
@@ -18,10 +18,10 @@ export class Mensaje {
 @Injectable()
 export class MensajesService {
   private mensajes = [
-    new Mensaje(1, Date.now(), 1, 2, 'mensaje 1', 0),
-    new Mensaje(3, Date.now() + 3, 3, 2, 'mensaje 2', 1),
-    new Mensaje(2, Date.now() + 2, 2, 1, 'mensaje 1.2', 1),
-    new Mensaje(4, Date.now() + 4, 4, 1, 'mensaje 3', 0)
+    new Mensaje(1, Date.now(), 1, 2, '¿aceptas cambio por una moto + dinero?', 0),
+    new Mensaje(3, Date.now() + 3, 3, 2, '¿Puedes quedar el viernes por la tarde?', 1),
+    new Mensaje(2, Date.now() + 2, 2, 1, '¿Es negociable?', 1),
+    new Mensaje(4, Date.now() + 4, 4, 1, '¿Cuanto pide por él?', 0)
   ];
   //sin inicializar seria un 0
   private lastId: number = 4;
