fd0f008cb19c0442a903cfcc062ea857402a91c2
Naugh <j.rodriguezbarr@alumnos.urjc.es>
2016-05-03 10:06:37 +0200

    enlazados mensajes con ids de usuarios
    falta quitar los mensajes los productos del usuario logueado

--- angular/src/app/product.list.component.ts
+++ angular/src/app/product.list.component.ts
@@ -51,9 +51,9 @@ export class ProductListComponent{
 
 
 
-  mensaje(){
+  mensaje(idUser){
     if(this.uService.getIdUserLogued()){
-      this.router.navigate(['Mensajes']);
+      this.router.navigate(['Mensajes', { id: idUser }]);
     }else{
       this.router.navigate(['Login']);
     }
