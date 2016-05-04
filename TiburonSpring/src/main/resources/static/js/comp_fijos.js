0cd11cc25aeb314f90ec3a0e35d68e03ee3f8a01
dvillatobas <dvd1880@gmail.com>
2016-05-04 16:16:09 +0200

    gitignore

--- TiburonSpring/src/main/resources/static/js/comp_fijos.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('nav').load('componentes/header.html');
-    $('#contenido').load('componentes/contenido.html');
-    $('.navbar-foot').load('componentes/footer.html');
-    
-    
-});
-
