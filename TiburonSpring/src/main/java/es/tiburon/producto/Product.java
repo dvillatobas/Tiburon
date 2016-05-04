49b3f6c68f663182317460f204c15e5a960caee5
CarlosSantos8 <4a.santos@gmail.com>
2016-05-04 11:28:52 +0200

    Clases productos

---
+++ TiburonSpring/src/main/java/es/tiburon/producto/Product.java
@@ -0,0 +1,98 @@
+package es.tiburon.producto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import org.springframework.data.annotation.Id;
+
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String publicDate;
+	private String name;
+	private int used;
+	private int year;
+	private String location;
+	//private String img;
+	private float price;
+	private int idUser;
+	private String type;
+	private String description;
+	
+	public Product(String date,String name,int used, int year,String location, float price,
+			int idUser, String type, String description){
+		this.publicDate = date;
+		this.name = name;
+		this.used = used;
+		this.year = year;
+		this.location = location;
+		this.price = price;
+		this.idUser = idUser;
+		this.type = type;
+		this.description = description;
+		
+	}
+	
+	
+	
+	public String getPublicDate() {
+		return publicDate;
+	}
+	public void setPublicDate(String publicDate) {
+		this.publicDate = publicDate;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getUsed() {
+		return used;
+	}
+	public void setUsed(int used) {
+		this.used = used;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public int getIdUser() {
+		return idUser;
+	}
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+
+}
---
+++ TiburonSpring/src/main/java/es/tiburon/producto/ProductController.java
@@ -0,0 +1,10 @@
+package es.tiburon.producto;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProductController {
+
+
+}
