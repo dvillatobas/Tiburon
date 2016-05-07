cdff8bde6595163f462e09a396072e6747597754
CarlosSantos8 <4a.santos@gmail.com>
2016-05-07 17:06:20 +0200

    Devolver producto concreto

--- TiburonSpring/src/main/java/es/tiburon/code/product/ProductController.java
+++ TiburonSpring/src/main/java/es/tiburon/code/product/ProductController.java
@@ -4,6 +4,8 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,6 +22,17 @@ public class ProductController {
 	@RequestMapping(value = "/", method = RequestMethod.GET )
 	public Collection<Product> getProducts(){
 		return pRepository.findAll();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Product> findProduct(@PathVariable long id){
+		Product founded = pRepository.findOne(id);
+		if(founded != null){
+			return new ResponseEntity<>(founded,HttpStatus.OK);
+		}
+		else{
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
 	
 	@RequestMapping(value ="/", method = RequestMethod.POST)
