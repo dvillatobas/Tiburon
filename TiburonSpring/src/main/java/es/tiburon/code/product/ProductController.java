806c9c149f468b902398c4152045ebf2564a05be
CarlosSantos8 <4a.santos@gmail.com>
2016-05-07 16:57:43 +0200

    Guardar nuevos productos

--- TiburonSpring/src/main/java/es/tiburon/code/product/ProductController.java
+++ TiburonSpring/src/main/java/es/tiburon/code/product/ProductController.java
@@ -3,8 +3,11 @@ package es.tiburon.code.product;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,4 +21,14 @@ public class ProductController {
 	public Collection<Product> getProducts(){
 		return pRepository.findAll();
 	}
+	
+	@RequestMapping(value ="/", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	public Product newProduct(@RequestBody Product producto){
+		
+		pRepository.save(producto);
+		return producto;
+	}
+	
+	
 }
