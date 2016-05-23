425213803dcf8d5cd91a5b1b7b5c1a1d8fa4be3d
Naugh <j.rodriguezbarr@alumnos.urjc.es>
2016-05-23 13:07:36 +0200

    Añadiendo más metodos en java

--- TiburonSpring/src/main/java/es/tiburon/code/mensaje/MensajeController.java
+++ TiburonSpring/src/main/java/es/tiburon/code/mensaje/MensajeController.java
@@ -5,8 +5,13 @@ import java.util.Collection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,9 +23,50 @@ public class MensajeController {
 	private MensajeRepository mRepo;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public Collection <Mensaje> getMensajeList(){
+	public Collection<Mensaje> getMensajeList(){
 		return mRepo.findAll();
 	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Mensaje> getMensaje(@PathVariable long id){
+		log.info("Get mensaje {}", id);
+		Mensaje m = mRepo.findOne(id);
+		if(m!=null){
+			return new ResponseEntity<>(m,HttpStatus.OK);
+		}else{
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	public Mensaje nuevoAnuncio(@RequestBody Mensaje mensaje) {
+		mRepo.save(mensaje);
+		return mensaje;
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Mensaje> getMensajeByEmisor(@PathVariable long id){
+		log.info("Get contact list {}", id);
+		Mensaje m = mRepo.findByIdEmisor(id);
+		if(m!=null){
+			return new ResponseEntity<>(m,HttpStatus.OK);
+		}else{
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Mensaje> getMensajeByReceptor(@PathVariable long id){
+		log.info("Get contact list {}", id);
+		Mensaje m = mRepo.findByIdEmisor(id);
+		if(m!=null){
+			return new ResponseEntity<>(m,HttpStatus.OK);
+		}else{
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
 	
 	
 }
--- TiburonSpring/src/main/java/es/tiburon/code/mensaje/MensajeRepository.java
+++ TiburonSpring/src/main/java/es/tiburon/code/mensaje/MensajeRepository.java
@@ -3,6 +3,6 @@ package es.tiburon.code.mensaje;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MensajeRepository extends JpaRepository<Mensaje, Long> {
+	Mensaje findByIdEmisor(Long Id);
 	
-
 }
